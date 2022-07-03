5e00db71a9e95f23e0288abf8b5ab0848b2694b2
geo <tp53.geo@gmail.com>
2022-07-03 02:30:58 +0200

    mise en place structure sass et finalisation factory photographer

--- scripts/factories/photographer.js
+++ scripts/factories/photographer.js
@@ -1,17 +1,27 @@
 function photographerFactory(data) {
-    const { name, portrait } = data;
+  const { id, name, city, country, tagline, price, portrait } = data;
 
-    const picture = `assets/photographers/${portrait}`;
+  const picture = `assets/photographers/${portrait}`;
 
-    function getUserCardDOM() {
-        const article = document.createElement( 'article' );
-        const img = document.createElement( 'img' );
-        img.setAttribute("src", picture)
-        const h2 = document.createElement( 'h2' );
-        h2.textContent = name;
-        article.appendChild(img);
-        article.appendChild(h2);
-        return (article);
-    }
-    return { name, picture, getUserCardDOM }
+  function getUserCardDOM() {
+    const content = `      
+          <a href="photographer.html?id=${id}" aria-label="${name}">
+            <div class="photographer__picture">
+              <img src="${picture}" alt="${name}" />
+            </div>        
+            <h2 class="photographer__title">${name}</h2>        
+          </a>
+            <div class="photographer__infos" tabindex="0">
+              <div class="photographer__infos__localisation">${city}, ${country}</div>
+              <div class="photographer__infos__description">${tagline}</div>
+              <div class="photographer__infos__pricing">${price}€/jour</div>
+            </div>
+      `;
+    const photographer = document.createElement("article");
+    photographer.classList.add("photographer");
+    photographer.innerHTML = content;
+
+    return photographer;
+  }
+  return { id, name, city, country, tagline, price, portrait, getUserCardDOM };
 }
