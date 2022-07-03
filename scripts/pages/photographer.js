abfbc08be0844d7bf706852ac4d376280e83bdaa
geo <tp53.geo@gmail.com>
2022-07-03 06:31:04 +0200

    avancement de la creation des composants sass

--- scripts/pages/photographer.js
+++ scripts/pages/photographer.js
@@ -1,4 +1,8 @@
 let params = new URL(document.location).searchParams;
 let id = params.get("id");
 
-console.log(id);
+const photographerInfos = document.querySelector("photographer-header__infos");
+const photographerPicture = document.querySelector(
+  "photographer-header__picture"
+);
+const photographerMedia = document.querySelector("");
