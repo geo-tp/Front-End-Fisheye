48b9b89d2100ef2ef76f819f105061ae020cd802
geo <tp53.geo@gmail.com>
2022-07-04 01:46:29 +0200

    creation lightbox html et css

---
+++ scripts/utils/lightboxModal.js
@@ -0,0 +1,9 @@
+function closeLightbox() {
+  const lightboxModal = document.querySelector(".lightbox-modal");
+  lightboxModal.style.display = "none";
+}
+
+function openLightBox() {
+  const lightboxModal = document.querySelector(".lightbox-modal");
+  lightboxModal.style.display = "block";
+}
