ec2e882087912fbeabe1ad3f9c8effa0e3f577ca
geo <tp53.geo@gmail.com>
2022-07-03 06:40:14 +0200

    ajout des fonctions pour recuperer un photographe et ses media

--- scripts/pages/photographer.js
+++ scripts/pages/photographer.js
@@ -6,3 +6,58 @@ const photographerPicture = document.querySelector(
   "photographer-header__picture"
 );
 const photographerMedia = document.querySelector("");
+
+function getPhotographerById(id) {
+  var photographer = await fetch(
+    "http://localhost:8000/Front-End-Fisheye/data/photographers.json"
+  )
+    .then((response) => {
+      if (response.ok) {
+        return response.json();
+      }
+
+      throw response;
+    })
+    .then((response) => {
+      for (let photographer of response.photographers) {
+        if (photographer.id === id) {
+          return response.photographers;
+        }
+      }
+
+      throw response;
+    })
+    .catch((err) => {
+      throw new Error("La requete api a echouée");
+    });
+
+  return photographer;
+}
+
+function getMediaByPhotographerId(id) {
+  var media = await fetch(
+    "http://localhost:8000/Front-End-Fisheye/data/photographers.json"
+  )
+    .then((response) => {
+      if (response.ok) {
+        return response.json();
+      }
+
+      throw response;
+    })
+    .then((response) => {
+      let photographerMedia = [];
+      for (let media of response.media) {
+        if (media.photographerId === id) {
+          photographerMedia.push(media);
+        }
+      }
+
+      return photographerMedia;
+    })
+    .catch((err) => {
+      throw new Error("La requete api a echouée");
+    });
+
+  return media;
+}
